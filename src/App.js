9e0a2d4802206ca9a682d0c8060d70ff49796c70
nadeemkhanrtm1 <nadeemkhanrtm@gmail.com>
2021-02-01 10:58:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import { useSelector, useDispatch} from "react-redux";
 
-function App() {
+const App = () => {
+  const state = useSelector(state => (state.counter))
+  const dispatch = useDispatch();
+  const inc = () => {
+      // setCounter(preValues => preValues+1);
+      dispatch({type:'INC'})
+  }
+  const dnc = () => {
+    // setCounter(preValues => preValues-1);
+    dispatch({type:'DNC'})
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={dnc}>-</button>
+      <span>{state}</span>
+      <button onClick={inc}>+</button>
     </div>
   );
 }
---
+++ src/store/reducers/rootReducer.js
@@ -0,0 +1,15 @@
+const initialState = {
+    counter:0
+}
+
+const rootReducer = (state = initialState,action) => {
+    switch(action.type){
+        case "INC":
+            return {counter:state.counter+1}
+        case "DNC":
+            return {counter:state.counter-1}
+        default:
+            return state
+    }
+}
+export default rootReducer;
